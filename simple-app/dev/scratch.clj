d680e226038718dc0b6159dd92cd25de5ea5a2b7
Francisco Viramontes <kidpollo@gmail.com>
2020-06-14 23:51:51 -0700

    Clojure north first draft

---
+++ simple-app/dev/scratch.clj
@@ -0,0 +1,11 @@
+(ns clojure.test)
+(run-tests 'simple-app.core-test)
+
+(ns simple-app.core)
+(hello "World")
+
+(str "foo") (str "bar") (str "baz")
+
+(str "Hello World")
+
+(str "Hello World")
---
+++ simple-app/src/simple_app/core.clj
@@ -0,0 +1,3 @@
+(ns simple-app.core)
+(defn hello [subject]
+  (prn (str "Hello " subject)))
