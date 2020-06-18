8d17a70d1d38c0ae705f9657c72bf0244894ed02
Francisco Viramontes <kidpollo@gmail.com>
2020-06-18 15:31:41 -0700

    run full buffers

--- simple-app/dev/scratch.clj
+++ simple-app/dev/scratch.clj
@@ -1,6 +1,17 @@
 (ns clojure.test)
-(run-tests 'prod-app.topology-test)
+(run-tests 'simple-app.core-test)
 
-(require '[prod-app.integration.fixtures]
-         '[prod-app.integration.journal]
-         '[prod-app.integration.runner])
+(ns simple-app.core)
+(hello "World")
+
+(str "foo")(+ 1)(prn "baz")
+
+["foo" "bar" "baz"]
+
+[["foo" "bar" "baz" "qux"]
+ ["1" "2" "3" "4"]
+ [1 2 3 4]
+ '(:one :two :three :four)
+ (take 4 (range))]
+
+(clojure.pprint/print-table [{:a 1 :b 2 :c 3} {:b 5 :a 7 :c "dog"}])
